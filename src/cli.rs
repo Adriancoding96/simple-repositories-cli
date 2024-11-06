63130e41053349866778ac158b214a93ed8a117c
Adriancoding96 <adrian.nilsson.coding@gmail.com>
2024-11-06 18:17:47 +0000

    ***Added*** - cli struct in cli.rs that derives functionality from Parser in the clap crate

--- src/cli.rs
+++ src/cli.rs
@@ -1,2 +1,26 @@
-fn run() {
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+pub struct Cli {
+
+    /*
+    * Allows user to pass argument with short flag -p, and long flag --path.
+    * If no argument is passed it defaults to '.'
+    */
+    #[arg(short, long, default_value = ".")]
+    pub path: String,
+    
+    /*
+    * Allow user to set verbose flag with -v or --verbose, for debugging capabilities
+    */
+    #[arg(short, long, action = clap::ArgAction::SetTrue)]
+    pub verbose: bool,
+
+    #[arg(short, long, default_value="no output specified")]
+    pub output: String,
 }
+
+pub fn parse_args() -> Cli {
+    Cli::parse()
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use simrep_cli::cli::parse_args;
+
 fn main() {
-    simrep_cli::cli::run();
+    let args = parse_args();
+
+    let path = args.path;
+    let verbose = args.verbose;
+    let output_format = args.output;
+
+    //For debugging purposes
+    println!("Path: {}", path);
+    println!("Verbose: {}", verbose);
+    println!("Output Format: {}", output_format);
 }
